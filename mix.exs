fd866abc2ecd3aa0e1dd8c563f49e4015023906e
Abby Malson <august.malson@gmail.com>
2026-04-19 01:30:43 +0200

    Bump version to trigger rebuild with runtime v0.6.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyChore.MixProject do
   def project do
     [
       app: :bot_army_chore,
-      version: "0.1.8",
+      version: "0.1.9",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),

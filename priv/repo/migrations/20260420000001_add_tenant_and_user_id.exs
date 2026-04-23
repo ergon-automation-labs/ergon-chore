c2fcb58f6b6512f642140b104d5fe4207ebcfd4f
Abby Malson <august.malson@gmail.com>
2026-04-23 13:13:11 -0600

    Fix migration: use proper unless blocks instead of inline conditionals

--- priv/repo/migrations/20260420000001_add_tenant_and_user_id.exs
+++ priv/repo/migrations/20260420000001_add_tenant_and_user_id.exs
@@ -5,19 +5,19 @@ defmodule BotArmyChore.Repo.Migrations.AddTenantAndUserId do
     default_tenant_id = "00000000-0000-0000-0000-000000000001"
 
     # Add tenant_id and user_id to tasks (idempotent)
-    alter table(:tasks) do
-      add(:tenant_id, :uuid, null: true) unless Ecto.Migration.column_exists?(:tasks, :tenant_id)
-      add(:user_id, :uuid, null: true) unless Ecto.Migration.column_exists?(:tasks, :user_id)
-    rescue
-      _ -> nil
+    unless Ecto.Migration.column_exists?(:tasks, :tenant_id) do
+      alter table(:tasks) do
+        add(:tenant_id, :uuid, null: true)
+        add(:user_id, :uuid, null: true)
+      end
+
+      create(index(:tasks, [:tenant_id]))
+      create(index(:tasks, [:user_id]))
+
+      execute(
+        "UPDATE tasks SET tenant_id = '#{default_tenant_id}'::uuid WHERE tenant_id IS NULL"
+      )
     end
-
-    create(index(:tasks, [:tenant_id])) unless Ecto.Migration.index_exists?(:tasks, [:tenant_id])
-    create(index(:tasks, [:user_id])) unless Ecto.Migration.index_exists?(:tasks, [:user_id])
-
-    execute(
-      "UPDATE tasks SET tenant_id = '#{default_tenant_id}'::uuid WHERE tenant_id IS NULL"
-    ) unless Ecto.Migration.column_exists?(:tasks, :tenant_id)
   end
 
   def down do
